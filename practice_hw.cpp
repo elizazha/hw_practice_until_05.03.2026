c44eee51ae141f1efded904028b7096e1d4fc28f
elizazha <lizokbella@mail.ru>
2026-03-04 22:08:40 +0300

    +main +comments

--- practice_hw.cpp
+++ practice_hw.cpp
@@ -37,4 +37,13 @@ T* merge(const T* a, size_t sa, const T* b, size_t sb, T* c)
 
 int main()
 {
+  int a[] = {1, 4, 6, 9};
+  int b[] = {2, 3, 5, 7, 8};
+  size_t sa = 4, sb = 5;
+  int c[9];
+  merge(a, sa, b, sb, c);
+  for (size_t i = 0; i < sa + sb; i++)
+  {
+    std::cout << c[i] << " ";
+  }
 }
